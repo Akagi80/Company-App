8d76e28c049d51e6a55c13b88cd0406d9bf97791
Sebastian Sikora <ssikora80@gmail.com>
2021-07-11 13:21:13 +0200

    Add MongoBD in departments.routes.js

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+// const db = require('./../db');
 const ObjectId = require('mongodb').ObjectId;
 
 /*
@@ -15,12 +15,22 @@ router.get('/departments', (req, res) => {
   });
 });
 
-router.get('/departments', (req, res) => {
-  res.json(db.departments);
-});
 /*
 router.get('/departments/random', (req, res) => {
   res.json(db.departments[Math.floor(Math.random() * db.length)]);
+});
+*/
+router.get('/departments/random', (req, res) => {
+  // losowanie jendego (size: 1) elementu // db.collection('employees').aggregate([ { $sample: { size: 3 } } ]);
+  req.db.collection('departments').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  });
+});
+
+/*
+router.get('/departments/:id', (req, res) => {
+  res.json(db.departments.find(item => item.id == req.params.id));
 });
 */
 router.get('/departments/:id', (req, res) => {
@@ -32,25 +42,46 @@ router.get('/departments/:id', (req, res) => {
   });
 });
 
-router.get('/departments/:id', (req, res) => {
-  res.json(db.departments.find(item => item.id == req.params.id));
-});
-
+/*
 router.post('/departments', (req, res) => {
   const { name } = req.body;
   db.departments.push({ id: 3, name })
   res.json({ message: 'OK' });
 });
+*/
+router.post('/departments', (req, res) => {
+  const { name } = req.body;
+  req.db.collection('departments').insertOne({ name: name }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  })
+});
 
+/*
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
   db = db.departments.map(item => (item.id == req.params.id) ? { ...item, name } : item );
   res.json({ message: 'OK' });
 });
-
+*/
+router.put('/departments/:id', (req, res) => {
+  const { name } = req.body;
+  req.db.collection('departments').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name }}, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
+});
+/*
 router.delete('/departments/:id', (req, res) => {
   db = db.departments.filter(item => item.id != req.params.id)
   res.json({ message: 'OK' });
+});
+*/
+router.delete('/departments/:id', (req, res) => {
+  req.db.collection('departments').deleteOne({ _id: ObjectId(req.params.id) }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
 module.exports = router;
