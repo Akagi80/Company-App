de5472932e215d4913f28cbcd4d19d6f81b90933
Sebastian Sikora <ssikora80@gmail.com>
2021-07-11 17:06:12 +0200

    Add Mongoose id to employees and products

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,43 +1,81 @@
 const express = require('express');
 const router = express.Router();
 // const db = require('./../db');
-const ObjectId = require('mongodb').ObjectId;
+// const ObjectId = require('mongodb').ObjectId;
+const Employee = require('../models/employee.model');
 
 /*
 router.get('/employees', (req, res) => {
   res.json(db.employees);
 });
-*/
+
 router.get('/employees', (req, res) => {
   req.db.collection('employees').find().toArray((err, data) => {
     if(err) res.status(500).json({ message: err });
     else res.json(data);
   });
 });
+*/
+router.get('/employees', async (req, res) => {
+  try {
+    res.json(await Employee.find());
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+});
 
 /*
 router.get('/employees/random', (req, res) => {
   res.json(db.employees[Math.floor(Math.random() * db.length)]);
 });
-*/
+
 router.get('/employees/random', (req, res) => {
   req.db.collection('employees').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
     if(err) res.status(500).json({ message: err });
     else res.json(data[0]);
   });
 });
+*/
+router.get('/employees/random', async (req, res) => {
+
+  try {
+    const count = await Employee.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Employee.findOne().skip(rand);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
 
 /*
 router.get('/employees/:id', (req, res) => {
   res.json(db.employees.find(item => item.id == req.params.id));
 });
-*/
+
 router.get('/employees/:id', (req, res) => {
   req.db.collection('employees').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
     if(err) res.status(500).json({ message: err });
     else if(!data) res.status(404).json({ message: 'Not found' });
     else res.json(data);
   });
+});
+*/
+router.get('/employees/:id', async (req, res) => {
+
+  try {
+    const dep = await Employee.findById(req.params.id);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*
@@ -46,13 +84,29 @@ router.post('/employees', (req, res) => {
   db.employees.push({ id: 3, firstName, lastName })
   res.json({ message: 'OK' });
 });
-*/
+
 router.post('/employees', (req, res) => {
   const { firstName, lastName } = req.body;
   req.db.collection('employees').insertOne({ firstName: firstName, lastName: lastName }, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   })
+});
+*/
+router.post('/employees', async (req, res) => {
+
+  try {
+
+    const { firstName, lastName, department } = req.body;
+    const newDepartment = new Employee({ firstName: firstName, lastName: lastName, department: department });
+    // mongoosowe save to odpowiednik instertOne z MongoDB
+    await newDepartment.save();
+    res.json({ message: 'OK' });
+
+  } catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*
@@ -61,7 +115,7 @@ router.put('/employees/:id', (req, res) => {
   db = db.employees.map(item => (item.id == req.params.id) ? { ...item, firstName, lastName } : item );
   res.json({ message: 'OK' });
 });
-*/
+
 router.put('/employees/:id', (req, res) => {
   const { firstName, lastName } = req.body;
   req.db.collection('employees').updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName: firstName, lastName: lastName }}, err => {
@@ -69,18 +123,52 @@ router.put('/employees/:id', (req, res) => {
     else res.json({ message: 'OK' });
   });
 });
+*/
+router.put('/employees/:id', async (req, res) => {
+  const { firstName, lastName, department } = req.body;
+
+  try {
+    const emp = await Employee.findById(req.params.id);
+    if(emp) {
+      await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department }});
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
 
 /*
 router.delete('/employees/:id', (req, res) => {
   db = db.employees.filter(item => item.id != req.params.id)
   res.json({ message: 'OK' });
 });
-*/
+
 router.delete('/employees/:id', (req, res) => {
   req.db.collection('employees').deleteOne({ _id: ObjectId(req.params.id) }, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   });
 });
+*/
+router.delete('/employees/:id', async (req, res) => {
+
+  try {
+    const dep = await Employee.findById(req.params.id);
+    if(dep) {
+      await Employee.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
+
 
 module.exports = router;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,43 +3,81 @@
 const express = require('express');
 const router = express.Router();
 // const db = require('./../db');
-const ObjectId = require('mongodb').ObjectId;
+// const ObjectId = require('mongodb').ObjectId;
+const Product = require('../models/product.model');
 
 /*
 router.get('/products', (req, res) => {
   res.json(db.products);
 });
-*/
+
 router.get('/products', (req, res) => {
   req.db.collection('products').find().toArray((err, data) => {
     if(err) res.status(500).json({ message: err });
     else res.json(data);
   });
 });
+*/
+router.get('/products', async (req, res) => {
+  try {
+    res.json(await Product.find());
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+});
 
 /*
 router.get('/products/random', (req, res) => {
   res.json(db.products[Math.floor(Math.random() * db.length)]);
 });
-*/
+
 router.get('/products/random', (req, res) => {
   req.db.collection('products').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
     if(err) res.status(500).json({ message: err });
     else res.json(data[0]);
   });
 });
+*/
+router.get('/products/random', async (req, res) => {
+
+  try {
+    const count = await Product.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Product.findOne().skip(rand);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
 
 /*
 router.get('/products/:id', (req, res) => {
   res.json(db.products.find(item => item.id == req.params.id));
 });
-*/
+
 router.get('/products/:id', (req, res) => {
   req.db.collection('products').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
     if(err) res.status(500).json({ message: err });
     else if(!data) res.status(404).json({ message: 'Not found' });
     else res.json(data);
   });
+});
+*/
+router.get('/products/:id', async (req, res) => {
+
+  try {
+    const dep = await Product.findById(req.params.id);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*
@@ -48,13 +86,29 @@ router.post('/products', (req, res) => {
   db.products.push({ id: 3, name, client })
   res.json({ message: 'OK' });
 });
-*/
+
 router.post('/products', (req, res) => {
   const { name, client } = req.body;
   req.db.collection('products').insertOne({ name: name, client: client }, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   })
+});
+*/
+router.post('/products', async (req, res) => {
+
+  try {
+
+    const { name, client } = req.body;
+    const newDepartment = new Product({ name: name, client: client });
+    // mongoosowe save to odpowiednik instertOne z MongoDB
+    await newDepartment.save();
+    res.json({ message: 'OK' });
+
+  } catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*
@@ -63,7 +117,7 @@ router.put('/products/:id', (req, res) => {
   db = db.products.map(item => (item.id == req.params.id) ? { ...item, name, client } : item );
   res.json({ message: 'OK' });
 });
-*/
+
 router.put('/products/:id', (req, res) => {
   const { name, client } = req.body;
   req.db.collection('products').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name, client: client }}, err => {
@@ -71,19 +125,51 @@ router.put('/products/:id', (req, res) => {
     else res.json({ message: 'OK' });
   });
 });
+*/
+router.put('/products/:id', async (req, res) => {
+  const { name, client } = req.body;
+
+  try {
+    const emp = await Product.findById(req.params.id);
+    if(emp) {
+      await Product.updateOne({ _id: req.params.id }, { $set: { name: name, client: client }});
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
 
 /*
 router.delete('/products/:id', (req, res) => {
   db = db.products.filter(item => item.id != req.params.id)
   res.json({ message: 'OK' });
 });
-*/
 
 router.delete('/products/:id', (req, res) => {
   req.db.collection('products').deleteOne({ _id: ObjectId(req.params.id) }, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   });
+});
+*/
+router.delete('/products/:id', async (req, res) => {
+
+  try {
+    const dep = await Product.findById(req.params.id);
+    if(dep) {
+      await Product.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 module.exports = router;
