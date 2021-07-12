31a47f4ad2a78dc2c2e94c47666ecb78011c066a
Sebastian Sikora <ssikora80@gmail.com>
2021-07-12 20:50:56 +0200

    Add post reference in employees.routes.js

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -18,7 +18,7 @@ router.get('/employees', (req, res) => {
 */
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -42,7 +42,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const emp = await Employee.findOne().skip(rand);
+    const emp = await Employee.findOne().populate('department').skip(rand);
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
@@ -68,7 +68,7 @@ router.get('/employees/:id', (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const emp = await Employee.findById(req.params.id);
+    const emp = await Employee.findById(req.params.id).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
